4289f4274a233c4999cc5566faba21c9507294ba
razorloves <razorloves@gmail.com>
2018-01-04 20:59:18 -0600

    marlin: update build fingerprint & desc to OPM1.171019.012
    Change-Id: Ib7c17e0b9d32b4f4d23ec374a1786735b3ca4e0f

--- aosip.mk
+++ aosip.mk
@@ -11,9 +11,6 @@ $(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/google/marlin/aosp_marlin.mk)
 
-# Inherit arm64 phone gapps
-$(call inherit-product-if-exists, vendor/gapps/gapps.mk)
-
 -include device/google/marlin/marlin/device-aosip.mk
 
 ## Device identifier. This must come after all inclusions
@@ -25,7 +22,7 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=marlin \
-    BUILD_FINGERPRINT=google/marlin/marlin:8.0.0/OPR6.170623.012/4283428:user/release-keys \
-    PRIVATE_BUILD_DESC="marlin-user 8.0.0 OPR6.170623.012 4283428 release-keys"
+    BUILD_FINGERPRINT=google/marlin/marlin:8.1.0/OPM1.171019.012/4470837:user/release-keys \
+    PRIVATE_BUILD_DESC="marlin-user 8.1.0 OPM1.171019.012 4470837 release-keys"
 
 $(call inherit-product-if-exists, vendor/google/marlin/marlin-vendor.mk)
